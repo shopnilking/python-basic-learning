8c9fa8a226492247fa216a810e77c1748c9d07e4
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-02-10 16:55:21 +0100

    add list item

---
+++ 22_add_list_item.py
@@ -0,0 +1,31 @@
+# Python - Add List Items
+# Append Items
+# To add an item to the end of the list, use the append() method:
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# Insert Items
+# To insert a list item at a specified index, use the insert() method.
+# The insert() method inserts an item at the specified index:
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
+# Note: As a result of the examples above, the lists will now contain 4 items.
+
+# Extend List
+# To append elements from another list to the current list, use the extend() method.
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+# print(tropical)
+
+# Another Example
+# thislist += tropical
+# thislist = thislist+tropical
+# print(thislist)
+# print(tropical)
