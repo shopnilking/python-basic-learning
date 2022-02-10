f7fab5423d6d00f6506d409c4158949247de8192
sagar <lpop08783@gmail.com>
2022-02-10 21:09:18 +0600

    list practise

---
+++ 19_list.py
@@ -0,0 +1,36 @@
+# thislPython Lists
+mylist = ["apple", "banana", "cherry"]
+# List
+# Lists are used to store multiple items in a single variable.
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# List Items
+# List items are ordered, changeable, and allow duplicate values.
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+# List Length
+# To determine how many items a list has, use the len() function:
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# List Items - Data Types
+# List items can be of any data type:
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+# A list can contain different data types:
+
+list1 = ["abc", 34, True, 40, "male"]
+
+# What is the data type of a list?
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
