6a25f57f7d2d00d1a8f91604e0c4f7e472b929de
sagar <lpop08783@gmail.com>
2022-02-24 21:47:03 +0600

    24_02_2022_tuples

---
+++ 24_02_2022/02_access_tuples.py
@@ -0,0 +1,42 @@
+# Python - Access Tuple Items
+# Access Tuple Items
+# You can access tuple items by referring to the index number, inside square brackets:
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[1])
+
+# Negative Indexing
+# Negative indexing means start from the end.
+
+# -1 refers to the last item, -2 refers to the second last item etc.
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[-1])
+
+# Range of Indexes
+# You can specify a range of indexes by specifying where to start and where to end the range.
+
+# When specifying a range, the return value will be a new tuple with the specified items.
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:5])
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[:4])
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:])
+
+# Range of Negative Indexes
+# Specify negative indexes if you want to start the search from the end of the tuple:
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[-4:-1])
+
+# Check if Item Exists
+# To determine if a specified item is present in a tuple use the in keyword:
+
+thistuple = ("apple", "banana", "cherry")
+# if "apple" in thistuple:
+#   print("Yes, 'apple' is in the fruits tuple")
+print("apple" in thistuple)
---
+++ 24_02_2022/05_loop_tuples.py
@@ -0,0 +1,20 @@
+# Python - Loop Tuples
+# Loop Through a Tuple
+# You can loop through the tuple items by using a for loop.
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in thistuple:
+#   print(x)
+  
+# Learn more about for loops in our Python For Loops Chapter.
+
+# Loop Through the Index Numbers
+# You can also loop through the tuple items by referring to their index number.
+# Use the range() and len() functions to create a suitable iterable.
+
+# thistuple = ("apple", "banana", "cherry")
+# print(len(thistuple))
+# for i in range(len(thistuple)):
+#     print(i)
+# for i in range(len(thistuple)):
+#   print(thistuple[i])
