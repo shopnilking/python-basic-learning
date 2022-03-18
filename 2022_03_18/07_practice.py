266b1b38dbde36a70a4e5ad6ba1056d4917ce065
sagar <lpop08783@gmail.com>
2022-03-18 16:46:13 +0600

    change

--- 2022_03_18/07_practice.py
+++ 2022_03_18/07_practice.py
@@ -1,11 +1,12 @@
 # 1 2 3 4 5 6 7 8 9 10
 
-x = input('Enter your name:')
-print('Hello, ' + x)
+n = input('Enter a number:')
+a = int(n)
+print (type(n))
 
-n = 11
+# n = 11
 
-# for x in range(10):
-#    print(x+1)
-for x in range(1,n):
-   print(x)
+# # for x in range(10):
+# #    print(x+1)
+# for x in range(1,n):
+#    print(x)
