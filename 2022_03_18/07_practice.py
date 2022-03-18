d00ea357d619e54e5cfcf8fd1c2530013f346237
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-03-18 12:28:23 +0100

    hw added

--- 2022_03_18/07_practice.py
+++ 2022_03_18/07_practice.py
@@ -1,5 +1,23 @@
 # 1 2 3 4 5 6 7 8 9 10
 # Print 1 to n
+from turtle import end_fill, st
+
+
 n = int(input('Enter a number:'))
+
+# for x in range(1, n+1):
+#     print(x, end=', ')
+
+# Method 1
 for x in range(1, n+1):
-    print(x, end=',')
+    if(x != n):
+        print(x, end=', ')
+    else:
+        print(x)
+
+# Method 2
+# newList = []
+# for x in range(1, n+1):
+#     newList.append(str(x))
+
+# print(', '.join(newList))
