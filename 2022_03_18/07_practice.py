ae2bdf5c56d68c500138a0fe0a4153937c78a0f0
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-03-18 11:53:47 +0100

    1 to n print

--- 2022_03_18/07_practice.py
+++ 2022_03_18/07_practice.py
@@ -1,12 +1,5 @@
 # 1 2 3 4 5 6 7 8 9 10
-
-n = input('Enter a number:')
-a = int(n)
-print (type(n))
-
-# n = 11
-
-# # for x in range(10):
-# #    print(x+1)
-# for x in range(1,n):
-#    print(x)
+# Print 1 to n
+n = int(input('Enter a number:'))
+for x in range(1, n+1):
+    print(x)
