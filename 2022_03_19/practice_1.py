40b3b0fe41de48754561396dafbf7ecaa45f3130
sagar <lpop08783@gmail.com>
2022-03-19 21:12:40 +0600

    change

---
+++ 2022_03_19/practice_1.py
@@ -0,0 +1,12 @@
+number1 = 50
+number2 = 70
+jog = number1+number2
+gun = number1*number2
+vag = number1/number2
+biyog = number1-number2
+
+print("The jog is: {}".format (jog))
+print("The gun is: {}".format (gun))
+print("The vag is: {}".format (vag))
+print("The biyog is: {}".format (biyog))
+
