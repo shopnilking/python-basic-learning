65483d49f26676312ce52c8b5be7949bd60fcb53
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-03-20 16:09:19 +0100

    changes

--- 2022_03_19/practice_3.py
+++ 2022_03_19/practice_3.py
@@ -1,5 +1,7 @@
 number = int(input('Enter a number:'))
 if number % 2 == 0:
     print(str(number) + " is Even")
+    # print("{} is Even".format(number))
 else:
     print(str(number) + " is odd")
+    # print("{} is odd".format(number))
