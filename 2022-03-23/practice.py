d937659908683daf5996ad55e1d0cc83e851a059
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-03-23 15:57:06 +0100

    set practice for today

---
+++ 2022-03-23/practice.py
@@ -0,0 +1,31 @@
+# This is Michael Jordan. He is 35 years old. He is a programmer.
+# This is John Rosales. He is 20 years old. He is a student.
+# This is Mark Guillen. He is 30 years old. He is a doctor.
+# This is KB Tonel. He is 27 years old. He is an engineer.
+
+users = [
+    {
+        'first_name': 'Michael',
+        'last_name': 'Jordan',
+        'age': '35',
+        'profession': 'programmer'
+    },
+    {
+        'first_name': 'John',
+        'last_name': 'Rosales',
+        'age': '20',
+        'profession': 'student'
+    },
+    {
+        'first_name': 'Mark',
+        'last_name': 'Guillen',
+        'age': '30',
+        'profession': 'doctor'
+    },
+    {
+        'first_name': 'KB',
+        'last_name': 'Tonel',
+        'age': '27',
+        'profession': 'engineer'
+    },
+]
