d85ec9a02c3caef39bd7963acd7749c873e67a6b
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-03-25 11:00:08 +0100

    string split

--- 2022_03_25/practice_1.py
+++ 2022_03_25/practice_1.py
@@ -3,5 +3,31 @@ text = "amar sonar bangla ami tomoay valobashi"
 # first = ["amar", "sonar", "bangla", "ami", "tomoay", "valobashi"]
 # second = ["amar", "ami"]
 
-result = text.split()
-print (result)
+# Method 1
+# words = text.split()
+# # print (words)
+
+# result = []
+# for word in words:
+#     # get single word
+#     # get first two letters from that single word
+#     # check the conditions
+#     # when conditon is true, append the word to result
+#     if(word[:2] == "am"):
+#         result.append(word)
+
+# print(result)
+
+# Method 2
+words = text.split()
+
+result = []
+for word in words:
+    # get single word
+    # get first two letters from that single word
+    # check the conditions
+    # when conditon is true, append the word to result
+    if(word.startswith("am")):
+        result.append(word)
+
+print(result)
