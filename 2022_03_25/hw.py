c4c5f03ae95944f512df4f61c9ad7460f4622b25
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-03-31 17:52:30 +0200

    hw solution

--- 2022_03_25/hw.py
+++ 2022_03_25/hw.py
@@ -10,11 +10,11 @@ text = "{'first_name': 'Michael','last_name': 'Jordan','age': '35','profession':
 # print(ageList)
 
 
-# list2 = text.split(',')
-# firstNameList = []
-# for x in list2:
-#     if x.startswith("{'first_name': '"):
-#         firstNameList.append(x[16:-1])
+list2 = text.split(',')
+firstNameList = []
+for x in list2:
+    if x.startswith("{'first_name': '"):
+        firstNameList.append(x[16:-1])
 # print(firstNameList)
 
 
@@ -23,11 +23,31 @@ lastNameList = []
 for x in list3:
     if x.startswith("'last_name': '"):
         lastNameList.append(x[14:-1])
-print(lastNameList)
+# print(lastNameList)
 
 
+# list4 = text.split(',')
+# fullNameList = []
+# for x in list4:
+#     if x.startswith("{'first_name': '"):
+#         firstName = x[16:-1]
+#     if x.startswith("'last_name': '"):
+#         lastName = x[14:-1]
+#         fullNameList.append(firstName + " " + lastName)
 
-# hw 
+# print(fullNameList)
+
+# fullNameList = []
+# for (x, y) in zip(firstNameList, lastNameList):
+#     fullNameList.append(x + " " + y)
+# print(fullNameList)
+
+fullNameList = []
+for x in range(len(firstNameList)):
+    fullNameList.append(firstNameList[x] + " " + lastNameList[x])
+
+
+# hw
 # 1. Create a list of first names
 # 2. Create a list of last names
 # 3. Create a list of full names
